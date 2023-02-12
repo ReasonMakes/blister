5199f1badac570cbdb4daae2e5ae7976b535412b
ReasonMakes <vaysym@gmail.com>
2023-02-12 13:52:35 -0500

    Add preliminary view movement update

--- Blister/Assets/Scripts/CameraMovement.cs
+++ Blister/Assets/Scripts/CameraMovement.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    private const float TAU = 6.2831853071f;
+
     private float translationSpeed = 3f;
     private float zoomSpeed = 1f;
+    private float translationSpeedEdging = 6f;
+    private float screenEdgeMargin = 5f;
+    private float rotationSensitivity = 0.1f;
+    private float rotationYaw = 0f;
+    private float rotationPitch = 0f;
     //private float rotationSensitivity = 1f;
     //private float rotationYaw = 0f;
     //private float rotationPitch = 0f;
@@ -20,27 +27,36 @@ public class CameraMovement : MonoBehaviour
     private void Update()
     {
         //TRANSLATE CAMERA POSITION
+        //Keys
+        Vector3 forwardFloating = new Vector3(transform.forward.x, 0f, transform.forward.z).normalized;
+        Vector3 rightFloating = new Vector3(transform.right.x, 0f, transform.right.z).normalized;
         Vector3 vector = Vector3.zero;
-
-        //First-person flying movement
-        //if (Input.GetKey(KeyCode.W)) { vector += transform.forward; }
-        //if (Input.GetKey(KeyCode.A)) { vector -= transform.right; }
-        //if (Input.GetKey(KeyCode.S)) { vector -= transform.forward; }
-        //if (Input.GetKey(KeyCode.D)) { vector += transform.right; }
-        //if (Input.GetKey(KeyCode.Space)) { vector += transform.up; }
-        //if (Input.GetKey(KeyCode.LeftControl)) { vector -= transform.up; }
-
-        //RTS movement
-        if (Input.GetKey(KeyCode.W)) { vector += Vector3.forward; }
-        if (Input.GetKey(KeyCode.A)) { vector -= Vector3.right; }
-        if (Input.GetKey(KeyCode.S)) { vector -= Vector3.forward; }
-        if (Input.GetKey(KeyCode.D)) { vector += Vector3.right; }
-        
+        if (Input.GetKey(KeyCode.W)) { vector += forwardFloating; }
+        if (Input.GetKey(KeyCode.A)) { vector -= rightFloating; }
+        if (Input.GetKey(KeyCode.S)) { vector -= forwardFloating; }
+        if (Input.GetKey(KeyCode.D)) { vector += rightFloating; }
         vector = vector.normalized;
-
         transform.position += vector * translationSpeed * transform.position.y * Time.deltaTime;
 
-        //RTS zoom
+        //Screen edging
+        if (Input.mousePosition.x <= screenEdgeMargin)
+        {
+            transform.position -= rightFloating * translationSpeedEdging * transform.position.y * Time.deltaTime;
+        }
+        else if (Input.mousePosition.x >= Screen.width - screenEdgeMargin)
+        {
+            transform.position += rightFloating * translationSpeedEdging * transform.position.y * Time.deltaTime;
+        }
+        if (Input.mousePosition.y <= screenEdgeMargin)
+        {
+            transform.position -= forwardFloating * translationSpeedEdging * transform.position.y * Time.deltaTime;
+        }
+        else if (Input.mousePosition.y >= Screen.height - screenEdgeMargin)
+        {
+            transform.position += forwardFloating * translationSpeedEdging * transform.position.y * Time.deltaTime;
+        }
+
+        //Zoom
         if ((Input.mouseScrollDelta.y < 0) //|| Input.GetKey(KeyCode.Space))
             && transform.position.y < 100f) {
             transform.position += Vector3.up * zoomSpeed;
@@ -50,7 +66,86 @@ public class CameraMovement : MonoBehaviour
             transform.position -= Vector3.up * zoomSpeed;
         }
 
-        ////ROTATE CAMERA VIEW ANGLE
+        //ROTATE CAMERA VIEW ANGLE
+        //Specified rotation point
+        if (Input.GetMouseButton(2)) {
+            //Get pivot point
+            //Vector3 grabbedVector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f);
+            Vector3 grabbedVector3 = new Vector3(Screen.width/2f, Screen.height/2f, 1.0f);
+            Vector3 screenPointToWorldPoint = Camera.main.ScreenToWorldPoint(grabbedVector3);
+            Vector3 cameraToGrabbed = (Camera.main.ScreenToWorldPoint(grabbedVector3) - Camera.main.transform.position).normalized;
+            Ray ray = new Ray(screenPointToWorldPoint, cameraToGrabbed);
+            if (Physics.Raycast(ray, out RaycastHit hit, 5000.0f, LayerMask.GetMask("Ground")))
+            {
+                //Debug ray path
+                Debug.DrawLine(screenPointToWorldPoint, hit.point, Color.blue, 1.0f);
+
+                //YAW
+                //Get properties before rotation
+                Vector3 localRotationEulerBefore = transform.localRotation.eulerAngles;
+
+                //Orbit
+                rotationYaw -= Input.GetAxisRaw("Mouse X") * rotationSensitivity;
+                rotationYaw %= TAU;
+                Vector3 floatingHitPoint = new Vector3(hit.point.x, Camera.main.transform.position.y, hit.point.z);
+                float distanceToFloatingPivotPoint = (Camera.main.transform.position - floatingHitPoint).magnitude;
+                transform.position = floatingHitPoint + new Vector3(
+                    Mathf.Cos(rotationYaw) * distanceToFloatingPivotPoint,
+                    0f,
+                    Mathf.Sin(rotationYaw) * distanceToFloatingPivotPoint
+                );
+
+                //Look at pivot point
+                Quaternion rotationToPivotPoint = Quaternion.LookRotation(hit.point - transform.position, Vector3.up);
+                if (rotationToPivotPoint != Quaternion.identity)
+                {
+                    //Look at pivot point
+                    transform.localRotation = rotationToPivotPoint;
+
+                    //Preserve pitch
+                    Vector3 localRotationEuler = transform.localRotation.eulerAngles;
+                    transform.localRotation = Quaternion.Euler(
+                        localRotationEulerBefore.x, //pitch
+                        localRotationEuler.y,
+                        localRotationEuler.z
+                    );
+                }
+
+                ////PITCH
+                ////Get properties before rotation
+                //localRotationEulerBefore = transform.localRotation.eulerAngles;
+                //
+                ////Orbit
+                //rotationPitch += Input.GetAxisRaw("Mouse Y") * rotationSensitivity;
+                //rotationPitch %= TAU;
+                //Vector3 groundCamera = new Vector3(Camera.main.transform.position.x, 0f, Camera.main.transform.position.z);
+                //Vector3 groundHitPoint = new Vector3(hit.point.x, 0f, hit.point.z);
+                //float distanceToGroundPivotPoint = (groundCamera - groundHitPoint).magnitude;
+                //transform.position = groundHitPoint + new Vector3(
+                //    Mathf.Cos(rotationYaw) * distanceToFloatingPivotPoint,
+                //    Mathf.Sin(rotationPitch) * distanceToGroundPivotPoint,
+                //    Mathf.Sin(rotationYaw) * distanceToFloatingPivotPoint
+                //);
+                //
+                ////Look at pivot point
+                //rotationToPivotPoint = Quaternion.LookRotation(hit.point - transform.position, Vector3.up);
+                //if (rotationToPivotPoint != Quaternion.identity)
+                //{
+                //    //Look at pivot point
+                //    transform.localRotation = rotationToPivotPoint;
+                //
+                //    //Preserve pitch
+                //    Vector3 localRotationEuler = transform.localRotation.eulerAngles;
+                //    transform.localRotation = Quaternion.Euler(
+                //        localRotationEuler.x, //pitch
+                //        localRotationEulerBefore.y,
+                //        localRotationEulerBefore.z
+                //    );
+                //}
+            }
+        }
+        
+        ////First-person rotation
         //float rotationInputX = Input.GetAxisRaw("Mouse X") * rotationSensitivity;
         //rotationYaw += rotationInputX;
         //float rotationInputY = -Input.GetAxisRaw("Mouse Y") * rotationSensitivity;
--- README.md
+++ README.md
@@ -1,9 +1,54 @@
-# blister
+# Blister
+An RTS/ARPG-like portfolio project
 
-Controls:
-WASD - Camera translation
-Scroll wheel - Camera zoom
-LMB - Select Unit
-LMB + LShift - Add unit to selection
-LControl + Shift - Remove unit from selection
-Hold LMB + drag mouse - Box select unit(s)
+<br />
+Collaborators
+<table>
+  <tr>
+    <td>Lead 3D Artist & Game Designer</td>
+    <td>clementine#4336</td>
+    <td>Zachary Pagano</td>
+  </tr>
+  <tr>
+    <td>Lead Programmer / Music & Sound Designer</td>
+    <td>Reason#0790</td>
+    <td>Michael Judge</td>
+  </tr>
+  <tr>
+    <td>Programmer</td>
+    <td>jstn#0033</td>
+    <td>Judge Justin</td>
+  </tr>
+</table>
+<br />
+Controls
+<table>
+  <tr>
+    <td>WASD</td>
+    <td>Camera translation</td>
+  </tr>
+  <tr>
+    <td>Scroll wheel</td>
+    <td>Camera zoom</td>
+  </tr>
+  <tr>
+    <td>Hold MMB + drag mouse <i>or</i> QE <i>or</i> arrow keys</td>
+    <td>Rotate view angle around pivot point on ground</td>
+  </tr>
+  <tr>
+    <td>LMB</td>
+    <td>Select unit</td>
+  </tr>
+  <tr>
+    <td>LMB + LShift</td>
+    <td>Add unit to selection</td>
+  </tr>
+  <tr>
+    <td>LControl + Shift</td>
+    <td>Remove unit from selection</td>
+  </tr>
+  <tr>
+    <td>Hold LMB + drag mouse</td>
+    <td>Box select unit(s)</td>
+  </tr>
+</table>
